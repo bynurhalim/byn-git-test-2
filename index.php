73357bbd7ed9c4ed83f2b71b865f5226b97f2915
bynurhalim <bynurhalim@gmail.com>
2021-10-30 17:14:21 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi laudantium, officiis eveniet adipisci officia saepe rem porro, assumenda aperiam illum fugiat. Fugiat dolor consequuntur pariatur quaerat ut adipisci placeat rerum.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
